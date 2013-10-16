d946916d5dc2d3074b61709a982dabe4285884d4
Bart Baker <babaker@deloitte.com>
2013-10-16 18:59:44 -0400

    UPdated bat script and presentation

--- compile.bat
+++ compile.bat
@@ -1,2 +1,3 @@
 "C:\Program Files (x86)\CodeBlocks\MinGW\bin\gcc.exe" -fno-strict-aliasing -g -O0 -Wall -Wstrict-prototypes -fPIC -I"C:/Python27/Lib/site-packages/numpy/core/include/numpy" -I"C:/Python27/include" -c funcs.c -o funcs.o
-"C:\Program Files (x86)\CodeBlocks\MinGW\bin\gcc.exe" -shared funcs.o -o _funcs_d.so -O1, -Bsymbolic-functions, -Bsymbolic-functions
+"C:\Program Files (x86)\CodeBlocks\MinGW\bin\gcc.exe" -shared funcs.o -Wl,-O1 -Wl,-Bsymbolic-functions,-Bsymbolic-functions -o _funcs_d.so
+
