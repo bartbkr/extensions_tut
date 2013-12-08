bff5c4bfa28dcbf8feb9af9bd4e3b42ee34c37c5
Bart Baker <babaker@deloitte.com>
2013-12-08 08:35:49 -0500

    Updated windows gcc compile script

--- compile.bat
+++ compile.bat
@@ -1,3 +1,2 @@
-"C:\Program Files (x86)\CodeBlocks\MinGW\bin\gcc.exe" -fno-strict-aliasing -g -O0 -Wall -Wstrict-prototypes -fPIC -I"C:/Python27/Lib/site-packages/numpy/core/include/numpy" -I"C:/Python27/include" -c funcs.c -o funcs.o
-"C:\Program Files (x86)\CodeBlocks\MinGW\bin\gcc.exe" -shared funcs.o -Wl,-O1 -Wl,-Bsymbolic-functions,-Bsymbolic-functions -o _funcs_d.so
-
+"C:\Program Files (x86)\CodeBlocks\MinGW\bin\gcc.exe" -mdll -O -Wall -IC:\python27\lib\site-packages\numpy\core\include\numpy -IC:\python27\include -IC:\python27\PC -c funcs.c -o funcs.o
+"C:\Program Files (x86)\CodeBlocks\MinGW\bin\gcc.exe" -shared -s funcs.o -LC:\python27\libs -LC:\python27\PCbuild -lpython27 -lmsvcr90 -o _funcs.pyd
